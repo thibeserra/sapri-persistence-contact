43d0b26a61f2d1cb1bb56856b9057d28ccc346ed
Thiago de Paula Beserra <thiago.depaula.beserra@gmail.com>
2018-07-21 19:25:56 -0300

    Criou moduloe sapri-persistence-contact

---
+++ src/main/java/br/com/sapri/SapriPersistenceContactApplication.java
@@ -0,0 +1,12 @@
+package br.com.sapri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapriPersistenceContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapriPersistenceContactApplication.class, args);
+	}
+}
